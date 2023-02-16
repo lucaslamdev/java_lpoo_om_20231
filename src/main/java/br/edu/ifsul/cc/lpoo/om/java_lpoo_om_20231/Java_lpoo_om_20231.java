ed7b48bb2c1f233f91e9cdc953cc448577e5d3c4
lucaslamdev <50436012+lucaslamdev@users.noreply.github.com>
2023-02-16 11:07:24 -0300

    update pom

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/om/java_lpoo_om_20231/Java_lpoo_om_20231.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.om.java_lpoo_om_20231;
+
+/**
+ *
+ * @author lucas
+ */
+public class Java_lpoo_om_20231 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
